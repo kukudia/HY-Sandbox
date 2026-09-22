afb137bf2ca3399d744e533129c0d0c57474391e
kukudia <xhx13877102015@gmail.com>
2026-09-22 17:53:14 +0800

    Add Block Info, move icons, and update prefab
    Introduce Assets/Scripts/Block/Info.cs to replace the simple Status script with richer block metadata (statuses, enums and placeholder checks). Move many icon assets from Assets/Icons to Assets/Art/Icons and add new status icons (broken-link, energy, mark, shield, warning). Update PowerGeneratingUnit.prefab: add UniversalAdditionalLightData components and adjust status light positions. Update Assembly-CSharp.csproj to remove Status.cs and include Info.cs. Remove the old Assets/Scripts/Block/Status.cs file.

---
+++ Assets/Scripts/Block/Info.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour
+{
+    [Header("Static")]
+    public string blockName;
+    public string description;
+    public int cost;
+    public int realMass;
+    public int realRequiredPower;
+    public int realOutputPower;
+    public List<Status> statuses;
+
+    [Header("Dynamic")]
+    public ConnectionStatus connectionStatus;
+    public DurabilityStatus durabilityStatus;
+    public PowerStatus powerStatus;
+
+    public void CheckConnectionStatus()
+    {
+
+    }
+
+
+    public void CheckDurabilityStatus()
+    {
+
+    }
+
+    public void CheckPowerStatus()
+    {
+
+    }
+}
+
+[Serializable]
+public class Status
+{
+    public string name;
+    public Sprite icon;
+}
+
+public enum ConnectionStatus
+{
+    Normal,
+    NoConnection,
+}
+
+public enum DurabilityStatus
+{
+    Normal,
+    Damaged,
+    Broken,
+}
+
+public enum PowerStatus
+{
+    Normal,
+    UnderPower,
+    NoPower,
+}
+
